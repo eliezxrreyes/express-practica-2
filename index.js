7b2daad648b53e11781a26f8ac3ec904b1173f50
Orlando Eliezer Lomas Reyes <eliezer.west23@gmail.com>
2026-03-24 21:53:44 -0700

    Implementacion de endpoint GET usuario

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ app.get('/', (req, res) => {
   res.send('Hola mundo desde Express lol xdddd Viva Jojos!!!');
 });
 
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Orlando Eliezer Lomas Reyes',
+    rol: 'Estudiante'
+  };
+
+  res.json(usuario);
+});
+
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
