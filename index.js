23680acf147a0a2a07d47715ce0055b474740a9c
Orlando Eliezer Lomas Reyes <eliezer.west23@gmail.com>
2026-04-14 12:46:19 -0700

    Primer SELECT de alumnos con PostgreSQL y Express

--- index.js
+++ index.js
@@ -2,27 +2,21 @@ const express = require('express');
 const pool = require('./db');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express lol xdddd Viva Jojos!!!');
+  res.send('API funcionando - Viva Jojos!!!');
 });
 
-app.get('/usuario', (req, res) => {
-  const usuario = {
-    id: 1,
-    nombre: 'Orlando Eliezer Lomas Reyes',
-    rol: 'Estudiante'
-  };
-
-  res.json(usuario);
+app.get('/alumnos', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM alumno');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar alumnos:', error);
+    res.status(500).json({ error: 'Error al obtener los alumnos' });
+  }
 });
-
-pool.connect()
-  .then(() => {
-    console.log('Conexión exitosa a PostgreSQL');
-  })
-  .catch((err) => {
-    console.error('Error de conexión', err);
-  });
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
